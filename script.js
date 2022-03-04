cdd058af101caefe5bf618fafeb0d24d9a50df87
Davide Pulci <davide.pulci@gmail.com>
2022-03-04 16:32:22 +0100

    chieste informazioni necessarie al cliente

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('JS OK!')
+// chiedere all'utente il numero di chilometri che vuole percorrere
+const yourKm = parseInt(prompt('quanti km vuoi percorrere?'));
+
+console.log(yourKm)
+
+// chiedere all'utente la sua età
+const yourAge = prompt('quale è la tua età?');
+
+console.log(yourAge)
