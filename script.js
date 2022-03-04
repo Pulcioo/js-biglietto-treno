c0fd61b69aace0bd1c5f469985c5c72deb48fcd7
Davide Pulci <davide.pulci@gmail.com>
2022-03-04 17:25:34 +0100

    calcolato il prezzo in base ai km da percorrere

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere
 const yourKm = parseInt(prompt('quanti km vuoi percorrere?'));
 
-console.log(yourKm)
+console.log(yourKm);
 
 // chiedere all'utente la sua età
-const yourAge = prompt('quale è la tua età?');
+const yourAge = parseInt(prompt('quale è la tua età?'));
 
-console.log(yourAge)
+console.log(yourAge);
+
+// assicurarsi che i dati siano inseriti correttamente
+if (isNaN(yourKm) || isNaN(yourAge)) {
+    alert('Hai sbagliato...Inserisci dati numerici!')
+}
+
+// calcolo il prezzo del biglietto sapendo che
+// è definito in base ai km (0.21 € al km)
+const euroPerKm = 0.21
+
+let ticketPrice = euroPerKm * yourKm;
+
+console.log('Il prezzo del tuo biglietto in base ai km è', ticketPrice)
